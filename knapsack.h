af22d8ccb6af3ffe67a4551eb4b0ee7cf92c472c
RushalleDiyaSP <144626131+RushalleDiyaSP@users.noreply.github.com>
2024-05-02 23:20:19 -0400

    Create knapsack.h

---
+++ knapsack.h
@@ -0,0 +1,11 @@
+// knapsack.h
+
+#ifndef KNAPSACK_H
+#define KNAPSACK_H
+
+void create_knapsack(const char *filename);
+void brute_force_knapsack(const char *filename);
+void dynamic_programming_knapsack(const char *filename);
+void greedy_knapsack(const char *filename);
+
+#endif
